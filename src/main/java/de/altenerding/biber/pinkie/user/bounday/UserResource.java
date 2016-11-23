d52ee1b7b38b7b798c2aabb5d3155c753cb4e9c8
Daniel <daniel.sagert@gmail.com>
2016-11-23 16:47:45 +0100

    Extended user resource and service to provide all users or a single user by id

--- src/main/java/de/altenerding/biber/pinkie/user/bounday/UserResource.java
+++ src/main/java/de/altenerding/biber/pinkie/user/bounday/UserResource.java
@@ -6,20 +6,34 @@ import javax.ws.rs.FormParam;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import java.util.List;
+import java.util.Optional;
 
 
 @Path("/users")
 @Produces(MediaType.APPLICATION_JSON)
 public class UserResource {
+    UserService userService = new UserService();
 
     @GET
-    @Path("/")
-    public Response getDefaultUserInJSON() {
-        UserService userService = new UserService();
-        User user = userService.getDefaultUser();
+    public Response getUsers() {
+        List<User> users = userService.getUsers();
+        return Response.ok(users).build();
+    }
+
+    @GET
+    @Path("{id}")
+    public Response getUser(@PathParam("id") long id) {
+        Optional<User> user = userService.getUser(id);
+
+        if (!user.isPresent()) {
+            return Response.status(Response.Status.NOT_FOUND).build();
+        }
+
         return Response.ok(user).build();
     }
 
--- src/main/java/de/altenerding/biber/pinkie/user/bounday/UserService.java
+++ src/main/java/de/altenerding/biber/pinkie/user/bounday/UserService.java
@@ -2,12 +2,33 @@ package de.altenerding.biber.pinkie.user.bounday;
 
 import de.altenerding.biber.pinkie.user.entity.User;
 
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Optional;
+
 public class UserService {
 
-	public User getDefaultUser() {
-		User user = new User();
-        user.setFirstName("Pinkie");
-        user.setLastName("The Brain");
-        return user;
-	}
+    public List<User> getUsers() {
+        User pinky = new User();
+        pinky.setId(1);
+        pinky.setFirstName("Pinky");
+        pinky.setLastName("Narf");
+
+        User brain = new User();
+        brain.setId(2);
+        brain.setFirstName("Brain");
+        brain.setLastName("Domination");
+
+        List<User> users = new LinkedList<>();
+        users.add(pinky);
+        users.add(brain);
+
+        return users;
+    }
+
+    public Optional<User> getUser(long id) {
+        return getUsers().stream()
+                .filter(user -> user.getId() == id)
+                .findFirst();
+    }
 }
