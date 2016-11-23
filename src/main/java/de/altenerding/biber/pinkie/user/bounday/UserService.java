d2b111a2cafc8e85e1772ba2a0a8844ae120b8e7
Daniel Sagert <daniel.sagert@petafuel.de>
2016-11-23 15:44:25 +0100

    Changed default user name

--- src/main/java/de/altenerding/biber/pinkie/user/bounday/UserService.java
+++ src/main/java/de/altenerding/biber/pinkie/user/bounday/UserService.java
@@ -6,8 +6,8 @@ public class UserService {
 
 	public User getDefaultUser() {
 		User user = new User();
-		user.setFirstName("JonFromREST");
-		user.setLastName("DoeFromREST");
-		return user;
+        user.setFirstName("Pinkie");
+        user.setLastName("The Brain");
+        return user;
 	}
 }
