81ae548fd13986de88cdeeeb1f4c219c1eb540a6
AlexanderPraegla <alexander.praegla@gmail.com>
2017-10-24 18:44:42 +0200

    Enum for configuration of the different folders for files

---
+++ src/main/java/de/altenerding/biber/pinkie/business/config/ImageFolder.java
@@ -0,0 +1,18 @@
+package de.altenerding.biber.pinkie.business.config;
+
+public enum ImageFolder {
+	IMAGE_OF_WEEK("/file/imageOfWeek/"),
+	TEAM_IMAGE("/file/teamImages/"),
+	PROFILE_IMAGE("/file/profileImages"),
+	DOCUMTENTS("/file/documents");
+
+	private final String name;
+
+	ImageFolder(String name) {
+		this.name = name;
+	}
+
+	public String getName() {
+		return name;
+	}
+}
