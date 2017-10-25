b5de5cfd2f7cadf13ff3920888f3d5d4ea32d5c9
AlexanderPraegla <alexander.praegla@gmail.com>
2017-10-25 12:18:27 +0200

    Added Logging to TeamProvider

--- src/main/java/de/altenerding/biber/pinkie/business/team/control/TeamProvider.java
+++ src/main/java/de/altenerding/biber/pinkie/business/team/control/TeamProvider.java
@@ -1,7 +1,9 @@
 package de.altenerding.biber.pinkie.business.team.control;
 
 import de.altenerding.biber.pinkie.business.team.entity.Team;
+import org.apache.logging.log4j.Logger;
 
+import javax.inject.Inject;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import java.util.List;
@@ -10,12 +12,20 @@ public class TeamProvider {
 
 	@PersistenceContext
 	private EntityManager em;
+	private Logger logger;
 
 	public List<Team> getTeams() {
+		logger.info("Loading all Teams from database");
 		return em.createNamedQuery("Team.findAll", Team.class).getResultList();
 	}
 
 	public Team getTeamById(long id) {
+		logger.info("Loading team with id={}", id);
 		return em.createNamedQuery("Team.findById", Team.class).setParameter("id", id).getSingleResult();
+	}
+
+	@Inject
+	public void setLogger(Logger logger) {
+		this.logger = logger;
 	}
 }
