46008ede43be063a06a8c5b46558a0d5ca542ed9
AlexanderPraegla <alexander.praegla@gmail.com>
2017-10-25 15:38:03 +0200

    Added reports of a season for each team

--- src/main/java/de/altenerding/biber/pinkie/presentation/team/TeamBean.java
+++ src/main/java/de/altenerding/biber/pinkie/presentation/team/TeamBean.java
@@ -1,5 +1,7 @@
 package de.altenerding.biber.pinkie.presentation.team;
 
+import de.altenerding.biber.pinkie.business.report.boundary.ReportService;
+import de.altenerding.biber.pinkie.business.report.entity.Report;
 import de.altenerding.biber.pinkie.business.team.boundary.TeamService;
 import de.altenerding.biber.pinkie.business.team.entity.Team;
 import org.apache.logging.log4j.Logger;
@@ -16,15 +18,20 @@ import java.util.List;
 public class TeamBean implements Serializable {
 
 	private TeamService teamService;
+	private ReportService reportService;
 	private Logger logger;
 
 	@ManagedProperty(value = "#{param.teamId}")
 	private long teamId;
 	private Team team;
 
+	private String test;
+	private List<Report> teamReports;
+
 	public void initTeam() {
 		logger.info("Loading team data for id={}", teamId);
 		team = teamService.getTeamById(teamId);
+		teamReports = reportService.getReportsForTeam(team.getId(), team.getSeason().getId());
 	}
 
 	public List<Team> getTeams() {
@@ -37,13 +44,18 @@ public class TeamBean implements Serializable {
 		this.teamService = teamService;
 	}
 
-	public Team getTeam() {
-		return team;
-	}
-
 	@Inject
 	public void setLogger(Logger logger) {
 		this.logger = logger;
+	}
+
+	@Inject
+	public void setReportService(ReportService reportService) {
+		this.reportService = reportService;
+	}
+
+	public Team getTeam() {
+		return team;
 	}
 
 	public long getTeamId() {
@@ -54,4 +66,19 @@ public class TeamBean implements Serializable {
 		this.teamId = teamId;
 	}
 
+	public String getTest() {
+		return test;
+	}
+
+	public void setTest(String test) {
+		this.test = test;
+	}
+
+	public void setTeamReports(List<Report> teamReports) {
+		this.teamReports = teamReports;
+	}
+
+	public List<Report> getTeamReports() {
+		return teamReports;
+	}
 }
