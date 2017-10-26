9c8f4a1c9d644139ce7a04476994c2ad1b9e7e97
AlexanderPraegla <alexander.praegla@gmail.com>
2017-10-26 11:02:25 +0200

    Created extra bean for conditional content style in standings

---
+++ src/main/java/de/altenerding/biber/pinkie/presentation/global/ContentStyleBean.java
@@ -0,0 +1,19 @@
+package de.altenerding.biber.pinkie.presentation.global;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+@ManagedBean
+@RequestScoped
+public class ContentStyleBean {
+
+	public static final String CLUB_NAME = "Altenerding";
+
+	public String svaTeamStyleStanding(String teamName) {
+		if (teamName.contains(CLUB_NAME)) {
+			return "font-weight:bold";
+		} else {
+			return "";
+		}
+	}
+}
