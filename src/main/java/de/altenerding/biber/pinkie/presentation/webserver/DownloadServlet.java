4116d10d6a5bdad15ed96ed66d95942a690df67c
AlexanderPraegla <alexander.praegla@gmail.com>
2017-10-27 11:42:40 +0200

    Inject FileService into DownloadServlet - Remove UploadServlet (for the moment)

--- src/main/java/de/altenerding/biber/pinkie/presentation/webserver/DownloadServlet.java
+++ src/main/java/de/altenerding/biber/pinkie/presentation/webserver/DownloadServlet.java
@@ -1,5 +1,6 @@
 package de.altenerding.biber.pinkie.presentation.webserver;
 
+import de.altenerding.biber.pinkie.business.file.boundary.FileService;
 import org.apache.logging.log4j.Logger;
 
 import javax.inject.Inject;
@@ -8,19 +9,17 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.File;
-import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.io.PrintWriter;
 import java.net.URLDecoder;
 import java.nio.file.Files;
+import java.nio.file.Path;
 
 @SuppressWarnings("Duplicates")
 @WebServlet(name = "DownloadServlet", urlPatterns = {"/file/*"})
 public class DownloadServlet extends HttpServlet {
 
-	@Inject
 	private Logger logger;
+	private FileService fileService;
 
 	/**
 	 * Handles the HTTP
@@ -36,48 +35,18 @@ public class DownloadServlet extends HttpServlet {
 			throws ServletException, IOException {
 		// Read parameter from form that contains the filename to download
 		try {
-			String resourceFolder = System.getProperty("resourceFolder");
-
-			if (resourceFolder == null || resourceFolder.isEmpty()) {
-				throw new ServletException("Please provide the VM Option \'-DresourceFolder=\'");
-			}
-
 			String fileName = URLDecoder.decode(request.getPathInfo().substring(1), "UTF-8");
-			String filePath = resourceFolder + File.separator + fileName;
-			logger.info("Download file with fileName={}", filePath);
-			// Call the download method with the given file
-			doDownload(request, response, filePath);
-		} catch (Exception e) {
-			PrintWriter writer = response.getWriter();
-			writer.println("You either did not specify a file to upload or are "
-					+ "trying to upload a file to a protected or nonexistent "
-					+ "location.");
-			writer.println("ERROR: " + e.getMessage());
 
-			logger.error("Problems during file upload. ", e);
+			Path path = fileService.download(fileName, response.getOutputStream());
+
+			response.setHeader("Content-Type", getServletContext().getMimeType(fileName));
+			response.setHeader("Content-Length", String.valueOf(Files.size(path)));
+			response.setHeader("Content-Disposition", "inline; filename=\"" + path.getFileName().toString() + "\"");
+		} catch (Exception e) {
+			logger.error("Problems during file download. ", e);
 			response.setContentType("text/html;charset=UTF-8");
 			response.setStatus(400);
 		}
-	}
-
-
-	/**
-	 *  Sends a file to the output stream.
-	 *
-	 *  @param request The request
-	 *  @param response The response
-	 *  @param fileName The name the browser should receive.
-	 */
-	private void doDownload( HttpServletRequest request, HttpServletResponse response,
-							 String fileName) throws IOException {
-		File file = new File(fileName);
-		if (!file.exists()) {
-			throw new FileNotFoundException(String.format("File %s does not exist", fileName));
-		}
-		response.setHeader("Content-Type", getServletContext().getMimeType(fileName));
-		response.setHeader("Content-Length", String.valueOf(file.length()));
-		response.setHeader("Content-Disposition", "inline; filename=\"" + file.getName() + "\"");
-		Files.copy(file.toPath(), response.getOutputStream());
 	}
 
 	/**
@@ -89,4 +58,14 @@ public class DownloadServlet extends HttpServlet {
 	public String getServletInfo() {
 		return "Short description";
 	}// </editor-fold>
+
+	@Inject
+	public void setLogger(Logger logger) {
+		this.logger = logger;
+	}
+
+	@Inject
+	public void setFileService(FileService fileService) {
+		this.fileService = fileService;
+	}
 }
