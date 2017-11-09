aeed4154e5ebbc64d76b016038068f423e71a6a0
AlexanderPraegla <alexander.praegla@gmail.com>
2017-11-09 12:01:05 +0100

    Added Admin and Press filter for logged in area

---
+++ src/main/java/de/altenerding/biber/pinkie/presentation/filter/AdminFilter.java
@@ -0,0 +1,39 @@
+package de.altenerding.biber.pinkie.presentation.filter;
+
+import de.altenerding.biber.pinkie.business.login.boundary.AuthenticateService;
+import de.altenerding.biber.pinkie.business.members.entity.Role;
+
+import javax.inject.Inject;
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebFilter(urlPatterns = {"/secure/dean/*", "/secure/referee/*"}, filterName = "AdminFilter")
+public class AdminFilter implements Filter{
+
+	@Inject
+	private AuthenticateService authenticateService;
+
+	@Override
+	public void init(FilterConfig filterConfig) throws ServletException {
+
+	}
+
+	@Override
+	public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
+		HttpServletRequest request = (HttpServletRequest) req;
+		HttpServletResponse response = (HttpServletResponse) res;
+		if (authenticateService.authenticateRole(Role.ADMIN)) {
+			chain.doFilter(request, response);
+		} else {
+			response.sendError(HttpServletResponse.SC_FORBIDDEN);
+		}
+	}
+
+	@Override
+	public void destroy() {
+
+	}
+}
---
+++ src/main/java/de/altenerding/biber/pinkie/presentation/filter/PressFilter.java
@@ -0,0 +1,39 @@
+package de.altenerding.biber.pinkie.presentation.filter;
+
+import de.altenerding.biber.pinkie.business.login.boundary.AuthenticateService;
+import de.altenerding.biber.pinkie.business.members.entity.Role;
+
+import javax.inject.Inject;
+import javax.servlet.*;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebFilter(urlPatterns = {"/secure/report/*", "/secure/team/*"}, filterName = "PressFilter")
+public class PressFilter implements Filter{
+
+	@Inject
+	private AuthenticateService authenticateService;
+
+	@Override
+	public void init(FilterConfig filterConfig) throws ServletException {
+
+	}
+
+	@Override
+	public void doFilter(ServletRequest req, ServletResponse res, FilterChain chain) throws IOException, ServletException {
+		HttpServletRequest request = (HttpServletRequest) req;
+		HttpServletResponse response = (HttpServletResponse) res;
+		if (authenticateService.authenticateRole(Role.PRESS)) {
+			chain.doFilter(request, response);
+		} else {
+			response.sendError(HttpServletResponse.SC_FORBIDDEN);
+		}
+	}
+
+	@Override
+	public void destroy() {
+
+	}
+}
