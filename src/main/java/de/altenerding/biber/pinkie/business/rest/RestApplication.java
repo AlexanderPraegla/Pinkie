5f640ceae8d8acdfdbe6adf971507b0aa4b725a5
Alex <alexander.praegla@gmail.com>
2018-03-30 11:03:47 +0200

    Exluded rest entry point until the release of the new android app

--- src/main/java/de/altenerding/biber/pinkie/business/rest/RestApplication.java
+++ src/main/java/de/altenerding/biber/pinkie/business/rest/RestApplication.java
@@ -3,6 +3,7 @@ package de.altenerding.biber.pinkie.business.rest;
 import javax.ws.rs.ApplicationPath;
 import javax.ws.rs.core.Application;
 
-@ApplicationPath("rest")
+// Excluded until release of the new app
+//@ApplicationPath("rest")
 public class RestApplication extends Application {
 }
