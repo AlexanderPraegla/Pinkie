0af2f875e999b0aa330bd9db252663a3733098e8
AlexanderPraegla <alexander.praegla@gmail.com>
2018-04-25 13:17:59 +0200

    Added notification when new album is created

--- src/main/java/de/altenerding/biber/pinkie/business/album/boundary/AlbumService.java
+++ src/main/java/de/altenerding/biber/pinkie/business/album/boundary/AlbumService.java
@@ -1,13 +1,18 @@
 package de.altenerding.biber.pinkie.business.album.boundary;
 
 import de.altenerding.biber.pinkie.business.album.entity.Album;
+import de.altenerding.biber.pinkie.business.notification.control.MessageSender;
+import de.altenerding.biber.pinkie.business.notification.entity.NotificationType;
+import de.altenerding.biber.pinkie.business.notification.entity.Placeholder;
 import org.apache.logging.log4j.Logger;
 
 import javax.ejb.Stateless;
 import javax.inject.Inject;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Stateless
 public class AlbumService {
@@ -15,6 +20,9 @@ public class AlbumService {
 	private EntityManager em;
 	@Inject
 	private Logger logger;
+	@Inject
+	private MessageSender messageSender;
+	;
 
 	public List<Album> getAlbums() {
 		logger.info("Loading all albums");
@@ -37,6 +45,10 @@ public class AlbumService {
 		logger.info("Creating album \'{}\'", album.getDescription());
 		em.persist(album);
 		em.flush();
+
+		Map<Placeholder, String> placeholders = new HashMap<>();
+		placeholders.put(Placeholder.ALBUM_TITLE, album.getDescription());
+		messageSender.sendNotifications(NotificationType.ALBUM_CREATED, placeholders);
 	}
 
 	public void updateAlbum(Album album) {
