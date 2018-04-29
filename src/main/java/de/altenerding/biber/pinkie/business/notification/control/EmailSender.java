7ccbd7bbb7dc35c8ee8ea0f9f25b918d16a4cdab
Alex <alexander.praegla@gmail.com>
2018-04-29 22:16:54 +0200

    Added email sender alias

--- src/main/java/de/altenerding/biber/pinkie/business/notification/control/EmailSender.java
+++ src/main/java/de/altenerding/biber/pinkie/business/notification/control/EmailSender.java
@@ -12,6 +12,7 @@ import javax.mail.Session;
 import javax.mail.Transport;
 import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeMessage;
+import java.io.UnsupportedEncodingException;
 import java.util.Date;
 
 public class EmailSender {
@@ -24,15 +25,17 @@ public class EmailSender {
 	public void send(@Observes Email email) {
 		MimeMessage message = new MimeMessage(session);
 		try {
-			message.setFrom(new InternetAddress(session.getProperty("mail.from")));
+            message.setFrom(new InternetAddress(session.getProperty("mail.from"), "Biber Info"));
 			InternetAddress[] address = {new InternetAddress(email.getRecipient())};
 			message.setRecipients(Message.RecipientType.TO, address);
 			message.setSubject(email.getSubject());
 			message.setSentDate(new Date());
 			message.setContent(email.getBody(), "text/html; charset=utf-8");
 			Transport.send(message, session.getProperty("mail.user"), session.getProperty("mail.password"));
-		} catch (MessagingException ex) {
-			logger.error("Error sending email", ex);
-		}
+        } catch (MessagingException ex) {
+            logger.error("Error sending email", ex);
+        } catch (UnsupportedEncodingException e) {
+            e.printStackTrace();
+        }
 	}
 }
