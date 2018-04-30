64f1dadb45c803a42fa056ea1cd1473c220ec939
Alex <alexander.praegla@gmail.com>
2018-04-30 23:36:08 +0200

    Remove notification settings before deleting member

--- src/main/java/de/altenerding/biber/pinkie/business/notification/control/NotificationSettingsProcessor.java
+++ src/main/java/de/altenerding/biber/pinkie/business/notification/control/NotificationSettingsProcessor.java
@@ -1,11 +1,16 @@
 package de.altenerding.biber.pinkie.business.notification.control;
 
+import de.altenerding.biber.pinkie.business.members.entity.Member;
+import de.altenerding.biber.pinkie.business.notification.entity.CommunicationType;
+import de.altenerding.biber.pinkie.business.notification.entity.GeneralNotificationSetting;
 import de.altenerding.biber.pinkie.business.notification.entity.NotificationSetting;
+import de.altenerding.biber.pinkie.business.notification.entity.ReportNotificationSetting;
 import org.apache.logging.log4j.Logger;
 
 import javax.inject.Inject;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import java.util.List;
 
 public class NotificationSettingsProcessor {
 
@@ -13,6 +18,8 @@ public class NotificationSettingsProcessor {
     private EntityManager em;
     @Inject
     private Logger logger;
+    @Inject
+    private NotificationSettingsProvider notificationSettingsProvider;
 
     public void deleteReportNotificationSetting(NotificationSetting notificationSetting) {
         notificationSetting = em.merge(notificationSetting);
@@ -32,5 +39,24 @@ public class NotificationSettingsProcessor {
         notificationSetting = em.merge(notificationSetting);
         em.flush();
         logger.info("Updated successfully report notification setting with id={}", notificationSetting.getId());
+    }
+
+    public void deleteReportNotificationSettingForMember(Member member) {
+        logger.info("Deleting all notification settings for alias={}", member.getAlias());
+        for (CommunicationType communicationType : CommunicationType.values()) {
+            List<GeneralNotificationSetting> generalNotificationSettingsByMember = notificationSettingsProvider.getGeneralNotificationSettingsByMember(member, communicationType);
+            List<ReportNotificationSetting> reportNotificationSettingsByMember = notificationSettingsProvider.getReportNotificationSettingsByMember(member, communicationType);
+
+            for (GeneralNotificationSetting generalNotificationSetting : generalNotificationSettingsByMember) {
+                generalNotificationSetting = em.merge(generalNotificationSetting);
+                em.remove(generalNotificationSetting);
+            }
+            for (ReportNotificationSetting reportNotificationSetting : reportNotificationSettingsByMember) {
+                reportNotificationSetting = em.merge(reportNotificationSetting);
+                em.remove(reportNotificationSetting);
+            }
+
+            em.flush();
+        }
     }
 }
