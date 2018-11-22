84ee8f1d0d481c853f365950f25617e3bc797456
Alexander Praegla <alexander.praegla@gmail.com>
2018-11-22 11:00:42 +0100

    #129 Fix - committed nonsense

--- src/main/java/de/altenerding/biber/pinkie/business/security/Secured.java
+++ src/main/java/de/altenerding/biber/pinkie/business/security/Secured.java
@@ -1,13 +1,9 @@
 package de.altenerding.biber.pinkie.business.security;
 
-import de.altenerding.biber.pinkie.business.members.entity.PermissionType;
-
-import javax.enterprise.util.Nonbinding;
 import javax.ws.rs.NameBinding;
 import java.lang.annotation.Retention;
 import java.lang.annotation.Target;
 
-import static de.altenerding.biber.pinkie.business.members.entity.PermissionType.UNSECURED;
 import static java.lang.annotation.ElementType.METHOD;
 import static java.lang.annotation.ElementType.TYPE;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
@@ -16,6 +12,4 @@ import static java.lang.annotation.RetentionPolicy.RUNTIME;
 @Retention(RUNTIME)
 @Target({TYPE, METHOD})
 public @interface Secured {
-    @Nonbinding
-    PermissionType value() default UNSECURED;
 }
